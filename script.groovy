501bc0523c6e33f72515909c759850410631b913
Abdelrahmanomar483 <132902352+Abdelrahmanomar483@users.noreply.github.com>
2024-06-12 14:56:18 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -4,12 +4,12 @@ def buildJar() {
 } 
 
 def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
-    }
+                    echo "building the docker image..."
+                    withCredentials([usernamePassword(credentialsId: 'd95e5bec-495a-49d1-90e5-3b52c8daf1a0', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                    sh 'docker build -t abdosalah663888/docker:jm2.0 .'
+                    sh "echo $PASS | docker login -u $USER --password-stdin"
+                    sh 'docker push abdosalah663888/docker:jm2.0'
+                    }
 } 
 
 def deployApp() {
